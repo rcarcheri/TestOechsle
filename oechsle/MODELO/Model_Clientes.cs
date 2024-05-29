e208279bf969169e5e74c23bade7c088795bc82d
rcarcheri <74160924+rcarcheri@users.noreply.github.com>
2024-05-29 18:33:55 -0500

    Update Model_Clientes.cs

--- oechsle/MODELO/Model_Clientes.cs
+++ oechsle/MODELO/Model_Clientes.cs
@@ -11,7 +11,7 @@ namespace oechsle.MODELO
       public int Id { get; set; }
       public string Nombre { get; set; }
       public string Apellidos { get; set; }
-      public strinf Fecha_nacimiento { get; set; }
+      public string Fecha_nacimiento { get; set; }
       public int Edad { get; set; }
     
     }
